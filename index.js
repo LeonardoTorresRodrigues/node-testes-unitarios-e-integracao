4ed747909b76253754f0ee1ded4a6f3d8e041f05
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-08-02 19:45:43 -0300

    feat: add jest first tests

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-import assert from 'node:assert/strict';
-
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const verifiqueSe = (valor) => {
-  const assercoes = {
-    ehExatamenteIgualA(esperado) {
-      assert.strictEqual(valor, esperado);
-    },
-  };
-  return assercoes;
+export {
+  somaHorasExtras,
+  calculaDescontos,
 };
-
-const teste = (titulo, funcaoDeTeste) => {
-  try {
-    funcaoDeTeste();
-    console.log(`${titulo} passou!`);
-  } catch {
-    console.error(`${titulo} não passou!!!`);
-  }
-};
-
-teste('somaHorasExtras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-
-  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
-
-teste('calculaDesconto', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-
-  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
-});
